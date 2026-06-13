8e8b5f2c39cafd2214cebab5ce8c65362316cec5
Dennes Njoroge <dennesmwangi55@gmail.com>
2026-06-13 16:28:00 +0300

    feat: enhance registration process with error handling and validation feedback

--- auth-system-frontend-new/src/pages/Register.jsx
+++ auth-system-frontend-new/src/pages/Register.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import api from "../api/axios";
+import { toast } from "react-toastify";
 
 function Register() {
   const [firstName, setFirstName] = useState("");
@@ -7,16 +9,50 @@ function Register() {
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
+  const [errors, setErrors] = useState({});
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    setErrors({});
+
+    try {
+      if (password !== repeatPassword) {
+        setErrors({ password: "Passwords do not match" });
+        return;
+      }
+
+      const { data } = await api.post("/api/v1/auth/register", {
+        firstName,
+        lastName,
+        emailAddress,
+        password,
+      });
+      toast.success(data.message);
+    } catch (error) {
+      if (error.response) {
+        if (error.response?.data?.errors) {
+          setErrors(error.response.data.errors);
+          return;
+        }
+
+        toast.error(
+          error.response?.data?.message ||
+            "Something went wrong. Please try again.",
+        );
+      } else if (error.request) {
+        toast.error("Network error. Please check your internet connection.");
+      } else {
+        toast.error("An unexpected error occurred.");
+      }
+    }
   };
 
   return (
     <div className="min-h-screen bg-gray-200 flex items-center justify-center px-4">
       <form
         onSubmit={handleSubmit}
-        className="bg-white w-full max-w-md rounded-xl shadow-md p-6 space-y-5"
+        className="bg-white w-full max-w-md rounded-xl shadow-md p-6 space-y-2"
       >
         <div>
           <h2 className="text-2xl font-bold text-gray-900">Register</h2>
@@ -42,6 +78,10 @@ function Register() {
             placeholder="John"
             required
           />
+
+          {errors.firstName && (
+            <span className="text-red-500">{errors.firstName}</span>
+          )}
         </div>
 
         <div className="space-y-2">
@@ -60,6 +100,10 @@ function Register() {
             placeholder="Doe"
             required
           />
+
+          {errors.lastName && (
+            <span className="text-red-500">{errors.lastName}</span>
+          )}
         </div>
 
         <div className="space-y-2">
@@ -78,6 +122,10 @@ function Register() {
             placeholder="example@mail.com"
             required
           />
+
+          {errors.emailAddress && (
+            <span className="text-red-500">{errors.emailAddress}</span>
+          )}
         </div>
 
         <div className="space-y-2">
@@ -96,6 +144,10 @@ function Register() {
             placeholder="********"
             required
           />
+
+          {errors.password && (
+            <span className="text-red-500">{errors.password}</span>
+          )}
         </div>
 
         <div className="space-y-2">
@@ -118,7 +170,7 @@ function Register() {
 
         <button
           type="submit"
-          className="bg-black text-white font-medium w-full px-3 py-2 rounded-lg cursor-pointer hover:opacity-90 transition"
+          className="bg-black text-white font-medium w-full mt-2 px-3 py-2 rounded-lg cursor-pointer hover:opacity-90 transition"
         >
           Register
         </button>
